846b28788d3eaf219db3ed8126a1c750ca1504d4
Louie Chan <lokyin0831@gmail.com>
2022-09-13 14:53:26 +0800

    link game.html

--- private/game.js
+++ private/game.js
@@ -5,6 +5,25 @@ const socket = io.connect(); // connect to socketIO
 let ctx
 let canvas
 let fillBucket = false
+
+////// get user icon
+async function getProfile() {
+  const res = await fetch('/user/me')
+  const result = await res.json()
+  console.log(result.data.user)
+  if (result.data.user.name) {
+      document.querySelector('.user-name').innerHTML = `${result.data.user.name}`
+      document.querySelector('.user-icon').innerHTML = `<img src="${result.data.user.picture}">`
+  } else {
+      document.querySelector('.user-name').innerHTML = `Welcome !!!`
+  }
+}
+getProfile()
+
+
+
+
+
 function changeColor(color) {
   if (color === 'red') {
     selectedColor = '#ff0000';
